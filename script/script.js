74db4d555a7f9431278e776465d71bbfc55f82b3
Coronado, Benjamin (Student) <bc245@students.waikato.ac.nz>
2024-08-02 16:07:46 +1200

    Add comments and identation

--- script/script.js
+++ script/script.js
@@ -1,44 +1,20 @@
 const mainRow = document.querySelector('.learn-http-grid')
+//add an event listener which adds/removes classes for the arrow animation
+//on the LEARN-HTTP grid, onclick
 mainRow.addEventListener('click',() => {
-  if(mainRow.classList.contains('special1')){
-    mainRow.classList.add('speciala');
-    mainRow.classList.remove('special1');
-    mainRow.classList.remove('specialShowFiles');
-    mainRow.classList.add('specialHideFiles');
-    console.log("a")
+  //add/remove class based on if the arrow has been clicked or not
+  if(mainRow.classList.contains('pointdown')){
+    mainRow.classList.add('pointright');
+    mainRow.classList.remove('pointdown');
   }
   else{
-    mainRow.classList.add('special1');
-    mainRow.classList.remove('speciala');
-    mainRow.classList.add('specialShowFiles');
-    mainRow.classList.remove('specialHideFiles');
-    console.log("b")
+    mainRow.classList.add('pointdown');
+    mainRow.classList.remove('pointright');
   }
 });
 
-// const subRow = document.querySelector('.html-grid-row');
-// const subRowList = document.querySelectorAll('.html-grid-row');
-
-// subRowList.forEach(subRow => {
-//   subRow.addEventListener('click', () => {
-//     subRow.classList.toggle('specialb');
-//     console.log("c")
-//  });
-// });
-
-// function ch(){
-//   this.classList.toggle('specialb');
-// }
-
-// var subRowList = document.querySelectorAll('.html-grid-row');
-// for(var x = subRowList.length -1; x>=0;x--){
-//   subRowList[x].onclick= ch;
-//   console.log("c")
-// }
-
+//add a toggle function for the html grid
 let p = document.querySelector('.learn-http-grid');
-
 function toggleClass(){
-  p.classList.toggle('specialanimation');
-  console.log("c")
+  p.classList.toggle('blockgrid');
 }
